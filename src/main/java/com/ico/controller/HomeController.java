6aab35504d31c6c7fd61afbb94ed603414025676
Jose Garcia <josegarcia19809@gmail.com>
2024-01-01 10:25:41 -0600

    Agregar tipos de datos simples al modelo y desplegarlos en la vista

--- src/main/java/com/ico/controller/HomeController.java
+++ src/main/java/com/ico/controller/HomeController.java
@@ -12,6 +12,16 @@ public class HomeController {
     public String mostrarHome(Model model) {
         model.addAttribute("mensaje", "Bienvenidos a empleos App");
         model.addAttribute("fecha", new Date());
+
+        String puesto = "Auxiliar de contabilidad";
+        Date fechaPub = new Date();
+        double salario = 9000;
+        boolean vigente = true;
+
+        model.addAttribute("puesto", puesto);
+        model.addAttribute("fecha", fechaPub);
+        model.addAttribute("salario", salario);
+        model.addAttribute("vigente", vigente);
         return "home";
     }
 }
