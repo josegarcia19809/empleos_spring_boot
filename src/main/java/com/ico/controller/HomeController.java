9336a834f9d486e9e48bea21504d5716bd4c0f23
Jose Garcia <josegarcia19809@gmail.com>
2023-12-31 22:01:01 -0600

    Agregar información al modelo para desplegarla en la vista

--- src/main/java/com/ico/controller/HomeController.java
+++ src/main/java/com/ico/controller/HomeController.java
@@ -1,12 +1,17 @@
 package com.ico.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Date;
 
 @Controller
 public class HomeController {
     @GetMapping("/")
-    public String mostrarHome() {
+    public String mostrarHome(Model model) {
+        model.addAttribute("mensaje", "Bienvenidos a empleos App");
+        model.addAttribute("fecha", new Date());
         return "home";
     }
 }
