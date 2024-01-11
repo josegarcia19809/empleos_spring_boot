c13d43d23f5b9d8994202ee7516127399bb7db06
Jose Garcia <josegarcia19809@gmail.com>
2024-01-11 08:42:18 -0600

    Anotación @PathVariable. Urls dinámicas

---
+++ src/main/java/com/ico/controller/VacantesController.java
@@ -0,0 +1,21 @@
+package com.ico.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/vacantes")
+public class VacantesController {
+
+    @GetMapping("/view/{id}")
+    public String verDetalle(@PathVariable("id") int idVacante, Model model) {
+        System.out.println("idVacante: " + idVacante);
+        model.addAttribute("idVacante", idVacante);
+
+        // TODO: Buscar los detalles de la vacante en la BD
+        return "vacantes/detalle";
+    }
+}
